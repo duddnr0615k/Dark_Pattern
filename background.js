2c25e03c62d3fa435f7eb2110af32558db1a3979
duddnr0615k <duddnr0615k@naver.com>
2022-10-25 01:26:22 +0900

    good

--- background.js
+++ background.js
@@ -19,11 +19,20 @@
 	// let test;
 	// test = setInterval(setBackGroundStart,1000);
 	
+
+
 	function setBackGroundColor(){
 	
 		// alert("선택정보에 대해 다시 한번 확인해 주세요.");
+
+		try{
 		let selected= [];
 		for (const element of document.querySelectorAll('label,a,span,li>div,p,h3,h1,h2,h4')) {
+			if(getComputedStyle(element,'::after').content.includes('(선택') || getComputedStyle(element,'::after').content.includes('( 선택') || getComputedStyle(element,'::after').content.includes('[선택') || getComputedStyle(element,'::after').content.includes('[ 선택')   ){
+				element.style.backgroundColor = 'yellow';
+
+			}
+
 			if (element.textContent.includes('선택')) {   
 				selected.push(element);		
 			}
@@ -37,10 +46,14 @@
 		}
 		
 	}
-	
-	
-	chrome.tabs.onUpdated.addListener(setBackGroundStart);
-	
+	catch{
+		null;
+	}	
+	}
+
+	const test = setInterval(setBackGroundStart,1000);	
+	chrome.tabs.onUpdated.addListener(test);
+	// document.addEventListener('hashchange', setBackGroundStart);
 	
 	
 	
