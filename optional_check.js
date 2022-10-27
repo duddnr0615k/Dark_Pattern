c818c8aab5f91c750f36e52b0c4d16477f604f2d
jadeluck19 <jadeluck19@gmail.com>
2022-10-27 17:38:28 +0900

    탈퇴포함

--- optional_check.js
+++ optional_check.js
@@ -27,20 +27,47 @@ function setBackGroundStart2(){
 function setBackGroundColor(){
 	
 	// alert("선택정보에 대해 다시 한번 확인해 주세요.");
-	let selected=[];
+
+	try{
+	const selected= [];
 	for (const element of document.querySelectorAll('label,a,span,li>div,p,h3,h1,h2,h4')) {
+		if(getComputedStyle(element,'::after').content.includes('(선택') || getComputedStyle(element,'::after').content.includes('( 선택') || getComputedStyle(element,'::after').content.includes('[선택') || getComputedStyle(element,'::after').content.includes('[ 선택')   ){
+			element.style.backgroundColor = 'yellow';
+
+		}
+
 		if (element.textContent.includes('선택')) {   
 			selected.push(element);		
 		}
 	}
 	for (const element2 of selected){
 		if (element2.textContent.includes('(선택') || element2.textContent.includes('( 선택') || element2.textContent.includes('[선택')|| element2.textContent.includes('[ 선택')){
-			element2.style.backgroundColor = 'yellow';
+			 element2.style.backgroundColor = 'yellow';
 			
 		}
 	
 	}
+
+	const fontArr=[];
+	for (const fontTest of document.querySelectorAll('a,button,p, a>span')){
+		fontArr.push(fontTest)
+	}
+	for (const fontTest2 of fontArr){
+
+		if(fontTest2.textContent.includes('탈퇴')){
+			if(fontTest2.textContent.length===4 || fontTest2.textContent.length===5){
+				fontTest2.style.fontSize ='35px';
+				fontTest2.style.color = 'red';
+				fontTest2.style.backgroundColor ='yellow';
+		}
+		
+		}
+	}
 	
+}
+catch{
+	null;
+}	
 }
 
 
